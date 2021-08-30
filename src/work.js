e264efe0573a418dadcd62278fd0c00b0c40e344
Frode-git <15272516991@163.com>
2021-08-30 22:08:43 +0800

    fix some bugs

--- src/work.js
+++ src/work.js
@@ -54,23 +54,24 @@ function addNote() {
   note.appendChild(content);
   note.appendChild(tools);
   li_note.appendChild(note);
-  // 获取已创建的note数量
-  var notes = document.querySelectorAll('.note');
+
   // add 点击触发添加便利贴事件
   add.onclick = function () {
     addNote();
-    notes[notes.length - 1].style.left = notes[notes.length - 1].offsetLeft + 20 + 'px';
-    notes[notes.length - 1].style.top = notes[notes.length - 1].offsetTop - 40 + 'px';
+    // 获取已创建的note数量，让新创建的note偏移，此处的notes获取必须放在函数内
+    var notes = document.querySelectorAll('.note');
+    notes[notes.length - 1].style.left = notes[notes.length - 2].offsetLeft + 20 + 'px';
+    notes[notes.length - 1].style.top = notes[notes.length - 2].offsetTop - 40 + 'px';
   };
   // remover点击触发删除便签事件，就是将父类的父类，即便签删除
   remover.addEventListener('click', removeNote);
 
   function removeNote() {
     // 始终保留一个note
+    // 获取已创建的note数量
+    var notes = document.querySelectorAll('.note');
     if (notes.length > 1) {
       this.parentElement.parentElement.remove();
-    } else {
-      notes[0].removeEventListener('click', removeNote);
     }
   };
   // 编辑工具背景
@@ -79,55 +80,40 @@ function addNote() {
       this.classList.toggle('showBg');
     });
   };
-  var w = 0;
-  weight.addEventListener('click', function () {
-    var selection = window.getSelection();
-    var text = selection.toString();
-    var start = selection.anchorOffset;
-    var end = selection.focusOffset;
-    var text = content.innerText;
-    console.log(content.innerText);
-    console.log(text);
-    console.log(start);
-    console.log(end);
-    content.innerHTML = text.substring(0, start) + '<b>' + text.substring(start, end) + '</b>' + text.substring(end);
+  // 给整个文档添加一个点击事件，当点击内容不是工具栏内的图标时，显示默认背景，注意循环的位置
+  document.addEventListener('click', function (e) {
+    for (var i = 0; i < tools.children.length; i++) {
+      if (e.target !== tools.children[i]) {
+        tools.children[i].style.backgroundColor = '#333333';
+      } else {
+        tools.children[i].style.backgroundColor = '#3D3D3D';
+      };
+    };
   });
+
+  // 工具栏中的按钮功能设置
+  weight.addEventListener('click', function () {
+    document.execCommand("bold");
+  });
+
   ita.addEventListener('click', function () {
-    var selection = window.getSelection();
-    var text = selection.toString();
-    var start = selection.anchorOffset;
-    var end = selection.focusOffset;
-    var text = content.innerText;
-    console.log(content.innerText);
-    console.log(text);
-    console.log(start);
-    console.log(end);
-    content.innerHTML = text.substring(0, start) + '<i>' + text.substring(start, end) + '</i>' + text.substring(end);
+    document.execCommand("italic");
   });
   bottom.addEventListener('click', function () {
-    var selection = window.getSelection();
-    var text = selection.toString();
-    var start = selection.anchorOffset;
-    var end = selection.focusOffset;
-    var text = content.innerText;
-    console.log(content.innerText);
-    console.log(text);
-    console.log(start);
-    console.log(end);
-    content.innerHTML = text.substring(0, start) + '<u>' + text.substring(start, end) + '</u>' + text.substring(end);
+    document.execCommand("underline");
   });
 
 
-  // // 点击此便签时，突出显示  有bug
-  // note.addEventListener('click', function (e) {
-  //   for (var i = 0; i < notes.length; i++) {
-  //     if (i != notes.indexOf(e.target.parentElement)) {
-  //       notes[i].style.zIndex = 9;
-  //     } else {
-  //       notes[i].style.zIndex = 99;
-  //     }
-  //   }
-  // })
+  // 点击此便签时，突出显示  有bug
+  note.addEventListener('click', function () {
+    // 获取已创建的note数量
+    var notes = document.querySelectorAll('.note');
+    for (var i = 0; i < notes.length; i++) {
+      notes[i].style.zIndex = 9;
+      console.log(notes[i].style.zIndex);
+    }
+    note.style.zIndex = 99;
+  })
   // 鼠标拖动便签移动
   header.addEventListener('mousedown', function (e) {
     // 获取鼠标在头部区域的位置
@@ -137,9 +123,26 @@ function addNote() {
     document.addEventListener('mousemove', move);
 
     function move(e) {
-      // 注意note本身的top和left值
-      note.style.left = e.pageX - x + 'px';
-      note.style.top = e.pageY - y + 'px';
+      // X 和 Y 表示便签新的left和top值
+      var X = e.pageX - x;
+      var Y = e.pageY - y;
+      // 阻止便签移除可视区，避免拖出去后，拖不进来
+      if (X <= -350) {
+        X = -350;
+      } else if (X >= screen.availWidth) {
+        X = screen.availWidth;
+      } {
+        X = X;
+      }
+      if (Y <= 0) {
+        Y = 0;
+      } else if (Y >= screen.availHeight - 150) {
+        Y = screen.availHeight - 150;
+      } else {
+        Y = Y;
+      }
+      note.style.left = X + 'px';
+      note.style.top = Y + 'px';
     };
     // 鼠标弹起时，将鼠标移动事件移除
     document.addEventListener('mouseup', function () {
@@ -151,7 +154,6 @@ function addNote() {
 // 点击li_note时，默认生成一个且只能生成一个便签，时间侦听器中的options对象，提供了once属性，指定事件绑定次数，true表示执行之后，会自动移除绑定
 li_note.addEventListener('click', function () {
   addNote();
-  this.children[0].style.display = 'block';
 }, {
   once: true
 });
